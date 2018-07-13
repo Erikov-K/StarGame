ccc7828f42da105f9e857424cba3fa28324d124e
Konstantin Erikov <k.a.erikov@gmail.com>
2018-07-13 12:48:21 +0700

    Refactoring. For easy intergation with Lesson, project files were replased. - Added modifications for android launcher. - Added shoot sound and background music.
     On branch master
     Changes to be committed:
    	new file:   android/assets/sounds/Star_Wars_-_Clon_Wars_(mp3.pm).mp3
    	new file:   android/assets/sounds/Star_Wars_-_Soundtrack_(DemoLat.net).mp3
    	new file:   android/assets/sounds/shoot.wav
    	modified:   android/src/com/mygdx/game/AndroidLauncher.java
    	new file:   android/src/ru/geekbrains/stargame/AndroidLauncher.java
    
     Changes not staged for commit:
    	modified:   android/AndroidManifest.xml
    	deleted:    android/src/com/mygdx/game/AndroidLauncher.java
    	deleted:    core/src/com/mygdx/game/base/ActionListener.java
    	deleted:    core/src/com/mygdx/game/base/Base2DScreen.java
    	deleted:    core/src/com/mygdx/game/base/ScaledTouchUpButton.java
    	deleted:    core/src/com/mygdx/game/base/Sprite.java
    	deleted:    core/src/com/mygdx/game/game/Star2DGame.java
    	deleted:    core/src/com/mygdx/game/math/MatrixUtils.java
    	deleted:    core/src/com/mygdx/game/math/Rect.java
    	deleted:    core/src/com/mygdx/game/math/Rnd.java
    	deleted:    core/src/com/mygdx/game/screen/GameScreen.java
    	deleted:    core/src/com/mygdx/game/screen/MenuScreen.java
    	deleted:    core/src/com/mygdx/game/sprite/Background.java
    	deleted:    core/src/com/mygdx/game/sprite/ButtonExit.java
    	deleted:    core/src/com/mygdx/game/sprite/ButtonPlay.java
    	deleted:    core/src/com/mygdx/game/sprite/Buttons.java
    	deleted:    core/src/com/mygdx/game/sprite/EnemyShip.java
    	deleted:    core/src/com/mygdx/game/sprite/MainShip.java
    	deleted:    core/src/com/mygdx/game/sprite/Star.java
    	deleted:    core/src/com/mygdx/game/utils/Regions.java
    	modified:   core/src/ru/geekbrains/stargame/base/Ship.java
    	modified:   core/src/ru/geekbrains/stargame/screen/GameScreen.java
    	modified:   core/src/ru/geekbrains/stargame/screen/MenuScreen.java
    	modified:   desktop/src/com/mygdx/game/desktop/DesktopLauncher.java

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -1,10 +1,10 @@
-package com.mygdx.game;
+package ru.geekbrains.stargame.game;
 
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.game.game.Star2DGame;
+import ru.geekbrains.stargame.game.Star2DGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
